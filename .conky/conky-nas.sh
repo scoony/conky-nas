18930be0eaf402a034ab9c23ec4f6bc5c9e20d18
GUY Jérôme <scoonydeus@gmail.com>
2020-10-27 16:42:14 +0100

    Update conky-nas.sh

--- .conky/conky-nas.sh
+++ .conky/conky-nas.sh
@@ -2,12 +2,12 @@
  
  ## CONFIG
  #########
- font_title=""
- font_standard=""
- font_extra=""
- txt_align_right=""
- txt_align_center=""
- user_avatar=""
+ font_title="\${font Ubuntu:bold:size=10}"
+ font_standard="\${font Noto Mono:normal:size=8}"
+ font_extra="\${font sans-serif:normal:size=8}"
+ txt_align_right="\${alignr}"
+ txt_align_center="\${alignc}"
+ user_avatar="~/.conky/avatar.png"
  transmission_login=""
  transmission_password=""
  transmission_ip=""
@@ -19,25 +19,27 @@
  ## DONT EDIT AFTER THIS
  #######################
  
-#### Load config (if exist)
-if [[ -f ~/.conky/conky-nas.conf ]]; then
-  source ~/.conky/conky-nas.conf
+## Load config (if exist)
+if [ -f ~/.conky/conky-nas.conf ]; then
+  echo "ok"
+## source ~/.conky/conky-nas.conf
 fi
 
-#### Check local language and apply MUI
+## Check local language and apply MUI
 os_language=$(locale | grep LANG | sed -n '1p' | cut -d= -f2 | cut -d_ -f1)
-if [[ -f ~/.conky/MUI/"$os_language".lang ]]; then
+if [ -f "~/.conky/MUI/"$os_language".lang" ]; then
   script_language=`echo "~/.conky/MUI/"$os_language".lang"`
 else
   script_language=`echo "~/.conky/MUI/default.lang"`
 fi
-source $script_language
+##source $script_language
 
 #### Let's start
-if [[ -f ~/conky/avatar.png ]]; then
-  echo "\${image ~/.conky/avatar.png -p 238,3 -s 60x60 -f 86400}"
+if [ -f ~/.conky/avatar.png ]; then
+  echo "\${image $user_avatar -p 238,3 -s 60x60 -f 86400}"
 fi
-echo "${voffset -16}${font sans-serif:bold:size=18}${alignc}${time %H:%M}${font}"
-echo "${voffset 4}${alignc}${time %A %d %B}"
-echo "${font}${voffset -4}"
-echo "${font sans-serif:bold:size=10}SYSTEM ${hr 2}"
+echo "\${voffset -10}\${font sans-serif:bold:size=18}\${alignc}\${time %H:%M}\${font}"
+echo "${txt_align_center}\${time %A %d %B}"
+echo "\${font}\${voffset -4}"
+echo "${font_title}SYSTEM \${hr 2}"
+
