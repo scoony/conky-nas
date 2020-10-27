739e3e37d25f5a679c11401a72e6d5a8df4b7a13
GUY Jérôme <scoonydeus@gmail.com>
2020-10-27 11:31:34 +0100

    Update conky-nas.sh

--- .conky/conky-nas.sh
+++ .conky/conky-nas.sh
@@ -1,3 +1,19 @@
  #/bin/bash
  
+ ## CONFIG
+ #########
+ font_title=""
+ font_standard=""
+ font_extra=""
+ txt_align_right=""
+ txt_align_center=""
+ user_avatar=""
+ transmission_login=""
+ transmission_password=""
+ transmission_ip=""
+ transmission_port=""
+ plex_ip=""
+ plex_port=""
  
+ ## DONT EDIT AFTER THIS
+ #######################
