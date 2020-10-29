f8257ef8ef24c0c3c2687f3f5a274d3755443e22
GUY Jérôme <scoonydeus@gmail.com>
2020-10-29 12:59:45 +0100

    Update conky-update.sh

--- .conky/conky-update.sh
+++ .conky/conky-update.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 remote_folder="https://raw.githubusercontent.com/scoony/conky-nas/main/.conky/"
-local_folder="/home/scoony/.conky/"
+local_folder="~/.conky/"
 
 file001="conky-nas.sh"
 file002="conky-update.sh"
 file003="MUI/default.lang"
 file004="MUI/fr.lang"
 
+
+user_home=`echo ~`
+user_path=${local_folder//\~/$user_home}
 for current_file in $file{001..999}; do
   remote_md5=`curl -s ${remote_folder}$current_file | md5sum | cut -f1 -d" "`
-  local_md5=`md5sum ${local_folder}$current_file | cut -f1 -d" "`
+  local_md5=`md5sum ${user_path}$current_file | cut -f1 -d" "`
   if [[ $remote_md5 == $local_md5 ]]; then
     echo "$current_file : No upgrade required"
   else
