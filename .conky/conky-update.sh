b81253ab6a2937f58147a967f8fb2c5aff7267fc
GUY Jérôme <scoonydeus@gmail.com>
2020-10-29 13:54:30 +0100

    Update conky-update.sh

--- .conky/conky-update.sh
+++ .conky/conky-update.sh
@@ -11,6 +11,9 @@ file004="MUI/fr.lang"
 
 user_home=`echo ~`
 user_path=${local_folder//\~/$user_home}
+if [[ -d "${user_path}MUI" ]]; then
+  mkdir "${user_path}MUI"
+fi
 for current_file in $file{001..999}; do
   remote_md5=`curl -s ${remote_folder}$current_file | md5sum | cut -f1 -d" "`
   local_md5=`md5sum ${user_path}$current_file | cut -f1 -d" "`
