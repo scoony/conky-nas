a91070a754a7f32bd4d57291ef094cec6823918e
GUY Jérôme <scoonydeus@gmail.com>
2020-10-31 05:08:28 +0100

    Update conky-nas.sh

--- .conky/conky-nas.sh
+++ .conky/conky-nas.sh
@@ -122,7 +122,11 @@ for drive in $drives ; do
     disk_total=`df $drive | sed 1d | awk '{print $2}'`
     disk_total_human=`df -Hl $mount_point | sed 1d | awk '{print $2}'`
     disk_usage=`df $drive | sed 1d | awk '{print $5}' | sed 's/%//'`
-    echo ${font_standard}${mount_point:0:18} ${txt_align_right}\${goto 120}"["$(printf "%04s" $disk_free_human)" / "$(printf "%03d" $disk_usage)"%] "\${execbar 6,102 echo $disk_usage}
+    disk_temp=`echo $user_pass | sudo -kS hddtemp $drive | awk '{ print $NF }' | sed 's/C//'`
+    echo ${font_standard}${mount_point:0:18} ${txt_align_right}\${goto 120}"["$(printf "%04s" $disk_free_human)" / "$(printf "%03d" $disk_usage)"%] "\${execbar 6,79 echo $disk_usage}
+    echo "${font_standard}\${voffset -12}\${color light grey}\${goto 286}\${execbar 9,20 echo "100"}\${color}"
+    echo "\${font Noto Mono:regular:size=6}\${voffset -13}\${goto 289}\${color black}$disk_temp\$color"
+
   fi
 done
 echo "\${font}\${voffset -4}"
