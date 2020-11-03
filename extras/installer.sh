1a15341477a0de46d6996fd0f2a9c73bea718a36
FRONTEAU Adrien <zouzou.is.reborn@hotmail.fr>
2020-11-03 11:23:42 +0100

    Update installer.sh

--- extras/installer.sh
+++ extras/installer.sh
@@ -4,14 +4,17 @@
 sudo apt update
 sudo apt install -y conky-all net-tools jq curl transmission-cli fonts-symbola fonts-noto-mono fonts-font-awesome
 
+remote_folder="https://raw.githubusercontent.com/scoony/conky-nas/main"
+local_folder="$HOME"
 
-mkdir -p ~/.conky/MUI
-wget --q ".conkyrc" -O "~/.conkyrc"
-chmod + x ~/.conkyrc
-wget --q "conky-update.sh" -O "~/.conky/conky-update.sh"
-chmod +x ~/.conky/conky-update.sh
-wget --q "conky-nas.conf" -O "~/.conky/conky-nas.conf"
-bash ~/.conky/conky-update.sh
+
+mkdir "$local_folder/.conky"
+wget --q "$remote_folder/.conkyrc" -O "$local_folder"
+chmod + x "$HOME/.conkyrc"
+wget --q "$remote_folder/.conky/conky-update.sh" -O "$local_folder/.conky"
+chmod +x "$local_folder/.conky/conky-update.sh"
+wget --q "$remote_folder/.conky/conky-nas.conf" -O "$local_folder/.conky"
+bash "$local_folder/.conky/conky-update.sh"
 
 ### add to boot
 ### start conky
