861adbbfc61cc91b8a50cd37f9a5c124a3bc865f
FRONTEAU Adrien <zouzou.is.reborn@hotmail.fr>
2020-11-15 20:25:04 +0100

    Fix installer

--- extras/installer.sh
+++ extras/installer.sh
@@ -15,6 +15,7 @@ if [[ "$check_language" == "404: Not Found" ]]; then
 fi
 source <(curl -s https://raw.githubusercontent.com/scoony/conky-nas/main/.conky/MUI/$os_language.lang)
 
+
 ### make sure it's not the root account
 eval 'echo -e "\e[43m-------------------- $mui_installer_title --------------------\e[0m"' $log_install_echo
 if [ "$(whoami)" == "root" ]; then
@@ -22,40 +23,42 @@ if [ "$(whoami)" == "root" ]; then
   exit 1
 fi
 
+
 ## apt update
 sudo apt update 2>/dev/null >> $log_install &
-pid=$!
+pid=`ps aux | grep "apt update" | sed '/grep/d'`
 spin='-\|/'
 i=0
-while kill -0 $pid 2>/dev/null
-  do
+while [ "$pid" != "" ]; do
   i=$(( (i+1) %4 ))
   printf "\r[  ] $mui_installer_apt_update ${spin:$i:1}"
   sleep .1
+  pid=`ps aux | grep "apt update" | sed '/grep/d'`
 done
 printf "$my_printf" && printf "\r"
 eval 'echo -e "[\e[42m\u2713 \e[0m] $mui_installer_apt_update_done"' $log_install_echo
 
+
 ## install applications
 sudo apt install -y conky-all acpi net-tools jq curl transmission-cli fonts-symbola fonts-noto-mono fonts-font-awesome libxml2-utils 2>/dev/null >> $log_install &
-pid=$!
+pid=`ps aux | grep "apt install" | sed '/grep/d'`
 spin='-\|/'
 i=0
-while kill -0 $pid 2>/dev/null
-  do
+while [ "$pid" != "" ]; do
   i=$(( (i+1) %4 ))
   printf "\r[  ] $mui_installer_apt_install ${spin:$i:1}"
   sleep .1
+  pid=`ps aux | grep "apt install" | sed '/grep/d'`
 done
 sudo apt install -f -y 2>/dev/null >> $log_install &
-pid=$!
+pid=`ps aux | grep "apt install" | sed '/grep/d'`
 spin='-\|/'
 i=0
-while kill -0 $pid 2>/dev/null
-  do
+while [ "$pid" != "" ]; do
   i=$(( (i+1) %4 ))
   printf "\r[  ] $mui_installer_apt_install ${spin:$i:1}"
   sleep .1
+  pid=`ps aux | grep "apt install" | sed '/grep/d'`
 done
 printf "$my_printf" && printf "\r"
 eval 'echo -e "[\e[42m\u2713 \e[0m] $mui_installer_apt_install_done"' $log_install_echo
@@ -77,11 +80,8 @@ for current_file in $file{001..999}; do
     printf "\r[  ] $mui_installer_wget basename $(basename ${current_file}) ... ${spin:$i:1}" 
     sleep .1
   done
-  if [[ "$current_file" =~ ".sh" ]]; then
-    chmod +x "${user_path}${current_file}"
-  fi
+  chmod +x "${user_path}${current_file}"
 done
-chmod +x "$HOME/.conky/conky-update"
 printf "$my_printf" && printf "\r"
 eval 'echo -e "[\e[42m\u2713 \e[0m] $mui_installer_wget_done"' $log_install_echo
 
@@ -89,6 +89,7 @@ eval 'echo -e "[\e[42m\u2713 \e[0m] $mui_installer_wget_done"' $log_install_echo
 ## launch of conky-update
 eval 'echo -e "[\e[42m\u2713 \e[0m] $mui_installer_conkyupdate"' $log_install_echo
 nohup "$user_path/.conky/conky-update" > /dev/null 2>/dev/null &
+
 
 ### add to boot
 eval 'echo -e "[\e[42m\u2713 \e[0m] $mui_installer_autostart_start"' $log_install_echo
@@ -105,4 +106,6 @@ NoDisplay=false
 X-GNOME-Autostart-enabled=true
 EOT
 
+
+## it's time to launch it
 nohup conky > /dev/null 2>/dev/null &
