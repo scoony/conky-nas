e3f198fbe263eef2fd83bd449dd366ee0df76228
GUY <scoonydeus@gmail.com>
2021-12-26 10:59:30 +0100

    Plex (extras)

--- .conky/conky-nas.sh
+++ .conky/conky-nas.sh
@@ -755,6 +755,10 @@ if [[ "$net_adapter" != "" ]]; then
       plexip_used=`curl --silent https://plex.tv/pms/:/ip`
       echo $font_standard"$mui_plexip_used"$txt_align_right$plexip_used" "
       ## end but if plexip_used == vpn then notifcould be great
+      plex_pid=`service plexmediaserver status | grep "Main PID" | awk '{print $3}'`
+      plex_prlimit=`prlimit --pid $plex_pid --as --output HARD | tail -n 1`
+      echo $font_standard"$mui_plex_pid"$txt_align_right$plex_pid" "
+      echo $font_standard"$mui_plex_prlimit"$txt_align_right$plex_prlimit" "
       plex_xml=`curl --silent http://$plex_ip:$plex_port/status/sessions?X-Plex-Token=$plex_token`
       plex_users=`echo $plex_xml | xmllint --format - | awk '/<MediaContainer size/ { print }' | cut -d \" -f2`
       echo $font_standard"$mui_plex_streams"$txt_align_right$plex_users" "
