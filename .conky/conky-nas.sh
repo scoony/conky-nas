1712852c0c286965d29fd331c55a94f401b3b314
Scoony <scoonydeus@gmail.com>
2026-09-25 21:04:53 +0200

    fix module cat friends

--- .conky/conky-nas.sh
+++ .conky/conky-nas.sh
@@ -434,7 +434,7 @@ if [[ "$services_list" != "" ]]; then
   fi
 fi
 
-cat ~/.conky/steam_friends_monitor.ext 2>/dev/null
+cat ~/.conky/*.friends.ext 2>/dev/null
 cat ~/.conky/ark_servers_monitor.ext 2>/dev/null
 
 #### CPU Block
